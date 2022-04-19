b7247cb75c00dee47505cf4477e1f93b271e95c0
chloe0b0 <chloezeiter@gmail.com>
2022-04-18 18:25:39 -0700

    Update API

--- src/MarkovChain.py
+++ src/MarkovChain.py
@@ -1,11 +1,15 @@
 ''' Implementation of a Markov Chain with arbitrary state datatypes '''
 import random
+from typing import Any, Iterable
 
 class MarkovChain(object):
     ''' A Markov Chain object for arbitrary state datatypes '''
     def __init__(self) -> None:
-        self.markov_chain = dict() # dict of dicts 
-    def fit(self, Seq: list) -> None:
+        self.markov_chain = dict() # dict of dicts
+    def fit(self, Seq: Iterable, wrap: bool = False) -> None:
+        ''' Given a list of states [s0, s1, ..., sn], where Sn 'leads to' Sn+1, fits the model. If wrap is true, the first element will be appended to the end '''
+        if wrap: Seq.append(Seq[0])
+
         for i in range(0, len(Seq) - 1):
             a, b = Seq[i], Seq[i + 1]
 
@@ -14,26 +18,27 @@ class MarkovChain(object):
             if b not in self.markov_chain[a]: self.markov_chain[a][b] = 0
 
             self.markov_chain[a][b] += 1
+    def sample(self, state: Any) -> Any | bool:
+        ''' Samples a state from the probability distribution of a given state. Returns False if there are no possible states '''
+        elems, p = list(self.markov_chain[state].keys()), list(self.markov_chain[state].values())
+        if not elems: return False
+
+        return random.choices(elems, p, k = 1)[0]
     def walk(self, iterations: int) -> list | bool:
-        ''' Probabalistic walk about the model '''
+        ''' Probabalistic walk about the model for N iterations '''
         if not self.markov_chain: return False
 
         state = random.choice(list(self.markov_chain.keys()))
         y = [state]
 
         for i in range(iterations):
-            elems, p = list(self.markov_chain[state].keys()), list(self.markov_chain[state].values())
-            state = random.choices(elems, p, k = 1)[0]
-
+            state = self.sample(state)
             y.append(state)
 
         return y
+    def distribution(self, state: Any) -> dict[Any, float]:
+        ''' Given a state, returns the probability distribution of all possible subsequent states as a dictionary '''
+        distr = self.markov_chain[state]
+        sigma = sum(distr.values())
 
-
-if __name__ == "__main__":
-    model = MarkovChain()
-
-    model.fit("This is a sentence and a good sentence".split())
-    print(model.markov_chain)
-
-    print(model.walk(5))
+        return {k: v / sigma for k,v in distr.items()}
